24d48cabc84cada26a1397b3860ff725cd07e356
Khrystyna Ursuliak <ursuliakkhrystyna@gmail.com>
2024-09-16 20:03:36 +0300

    Refactor showBookOfTheDay function to store and retrieve book of the day from local storage

--- task-1/script.js
+++ task-1/script.js
@@ -8,11 +8,32 @@ const books = [
   'The Great Gatsby by F. Scott Fitzgerald'
 ]
 
+function getTodayDate() {
+  const today = new Date();
+  return today.toISOString().split('T')[0];
+}
+
 function showBookOfTheDay() {
+  /*
   const randomIndex = Math.floor(Math.random() * books.length);
   const book = books[randomIndex];
   document.getElementById('bookOfTheDay').textContent = `Today's Book of the Day is: ${book}`;
+  */
+  const todayDate = getTodayDate(); 
+  const savedDate = localStorage.getItem('savedDate');
+  let bookOfTheDay = localStorage.getItem('bookOfTheDay');
+
+  if (savedDate !== todayDate || !bookOfTheDay) {
+    const randomIndex = Math.floor(Math.random() * books.length);
+    bookOfTheDay = books[randomIndex];
+  
+    localStorage.setItem('bookOfTheDay', bookOfTheDay);
+    localStorage.setItem('savedDate', todayDate);
+  }
+
+  document.getElementById('bookOfTheDay').textContent = `Today's Book of the Day is: ${bookOfTheDay}`;
 }
+
 
 
 const todaysBookBtn = document.getElementById('todaysBook');
