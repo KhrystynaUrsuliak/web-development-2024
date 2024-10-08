a02f159c9659d3562ebbe9b7606c595063f3efe2
Khrystyna Ursuliak <ursuliakkhrystyna@gmail.com>
2024-10-08 09:27:34 +0300

    Updated Task 0

---
+++ task-0/02-java-fundamentals/13-loops.js
@@ -0,0 +1,17 @@
+// // Task 1
+// let i = 3;
+
+// while (i) {
+//   alert(i--);
+// } // alert will show 1
+
+// Task 2.1 - from 1 to 4
+// let i = 0;
+// while (++i < 5) alert(i); 
+
+// // Task 2.2 - from 1 to 5
+// let i = 0;
+// while (i++ < 5) alert(i); 
+
+// Task 3
+for (let i = 0; i < 5; i++) alert(i); 
