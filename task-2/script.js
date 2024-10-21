19f63e2f0ac4e048da11ae9a88ec835a34147642
Khrystyna Ursuliak <ursuliakkhrystyna@gmail.com>
2024-10-22 01:20:18 +0300

    Update Task 2

--- task-2/script.js
+++ task-2/script.js
@@ -1,24 +1,45 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const sp500PriceEl = document.getElementById('sp500-price');
-  const updateTimeEl = document.getElementById('update-time');
+let tasks = []
+const taskInput = document.getElementById('task-input')
+const addTaskBtn = document.getElementById('add-task-btn')
+const taskList = document.getElementById('task-list')
+const clearTasksBtn = document.getElementById('clear-tasks-btn')
+const savedTasks = JSON.parse(localStorage.getItem('tasks'))
 
-  const apiUrl = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=^GSPC&apikey=96JR8M3B00R55E3C`;
+if (savedTasks) {
+  tasks = savedTasks
+  renderTasks()
+}
 
-  async function fetchSP500Data() {
-    try {
-      const response = await fetch(apiUrl);
-      const data = await response.json();
-
-      const sp500Price = data.price;
-      const updateTime = new Date().toLocaleTimeString;
-    
-      sp500PriceEl.textContent = `S&P 500: $${sp500Price}`;
-      updateTimeEl.textContent = `Updated: ${updateTime}`;
-    } catch (error) {
-      sp500PriceEl.textContent = 'Could not fetch data';
-    }
+addTasksBtn.addEventListener('click', function() {
+  const task = taskInput.value
+  if (task) {
+    tasks.push(task)
+    taskInput.value = ''
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+    renderTasks()
   }
+})
 
-  fetchSP500Data();
-  setInterval(fetchSP500Data, 60000);
-});
+clearTasksBtn.addEventListener("click", function() {
+  localStorage.clear()
+  tasks = []
+  renderTasks(tasks)
+})
+
+function renderTasks(tasks) {
+  taskList.innerHTML = ""
+  tasks.forEach((task, index) => {
+      const taskItem = document.createElement("li")
+      taskItem.innerHTML = `
+          <span>${task}</span>
+          <button onclick="deleteTask(${index})">Delete</button>
+      `
+      taskList.appendChild(taskItem)
+  })
+}
+
+function deleteTask(index) {
+  tasks.splice(index, 1)
+  localStorage.setItem("tasks", JSON.stringify(tasks))
+  renderTasks(tasks)
+}
