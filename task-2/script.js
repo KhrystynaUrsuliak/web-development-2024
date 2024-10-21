4f50faa232c9f8d1c6674b8fd3661c0a0caebafe
Khrystyna Ursuliak <ursuliakkhrystyna@gmail.com>
2024-10-22 01:25:32 +0300

    Updated Task 2

--- task-2/script.js
+++ task-2/script.js
@@ -1,45 +1,45 @@
 let tasks = []
-const taskInput = document.getElementById('task-input')
-const addTaskBtn = document.getElementById('add-task-btn')
-const taskList = document.getElementById('task-list')
-const clearTasksBtn = document.getElementById('clear-tasks-btn')
-const savedTasks = JSON.parse(localStorage.getItem('tasks'))
+const taskInput = document.getElementById("task-input")
+const addTaskBtn = document.getElementById("add-task-btn")
+const taskList = document.getElementById("task-list")
+const clearTasksBtn = document.getElementById("clear-tasks-btn")
+const savedTasks = JSON.parse(localStorage.getItem("tasks"))
 
 if (savedTasks) {
-  tasks = savedTasks
-  renderTasks()
+    tasks = savedTasks
+    renderTasks(tasks)
 }
 
-addTasksBtn.addEventListener('click', function() {
-  const task = taskInput.value
-  if (task) {
-    tasks.push(task)
-    taskInput.value = ''
-    localStorage.setItem('tasks', JSON.stringify(tasks))
-    renderTasks()
-  }
+addTaskBtn.addEventListener("click", function() {
+    const task = taskInput.value
+    if (task) {
+        tasks.push(task)
+        taskInput.value = ""
+        localStorage.setItem("tasks", JSON.stringify(tasks))
+        renderTasks(tasks)
+    }
 })
 
 clearTasksBtn.addEventListener("click", function() {
-  localStorage.clear()
-  tasks = []
-  renderTasks(tasks)
+    localStorage.clear()
+    tasks = []
+    renderTasks(tasks)
 })
 
 function renderTasks(tasks) {
-  taskList.innerHTML = ""
-  tasks.forEach((task, index) => {
-      const taskItem = document.createElement("li")
-      taskItem.innerHTML = `
-          <span>${task}</span>
-          <button onclick="deleteTask(${index})">Delete</button>
-      `
-      taskList.appendChild(taskItem)
-  })
+    taskList.innerHTML = ""
+    tasks.forEach((task, index) => {
+        const taskItem = document.createElement("li")
+        taskItem.innerHTML = `
+            <span>${task}</span>
+            <button onclick="deleteTask(${index})">Delete</button>
+        `
+        taskList.appendChild(taskItem)
+    })
 }
 
 function deleteTask(index) {
-  tasks.splice(index, 1)
-  localStorage.setItem("tasks", JSON.stringify(tasks))
-  renderTasks(tasks)
+    tasks.splice(index, 1)
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+    renderTasks(tasks)
 }
